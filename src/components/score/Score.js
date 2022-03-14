86556cf723b1913c308c90e6fd861b1ec3b87ee2
Guillermo <guillef33@gmail.com>
2022-03-14 11:48:02 -0300

    react share whatsapp button in score component

--- src/components/score/Score.js
+++ src/components/score/Score.js
@@ -2,7 +2,13 @@ import React, { useContext } from 'react';
 
 import { AppContext } from "../../context/AppContext";
 
+import RaicesBack3 from "../../assets/back3.jpeg";
+
+import { WhatsappShareButton, WhatsappIcon } from "react-share";
+
 function Score() {
+
+  const shareUrl = "https://api.whatsapp.com/send?phone=1167062068&text=Ven%20a%20jugar%20con%20Bodega%20Raices.%20Cuanto%20sabes%20de%20los%20vinos%20espanoles"
 
     const {
     questions,
@@ -12,11 +18,12 @@ function Score() {
 
   return (
     <div className="score-section">
+      <img src={RaicesBack3} alt="sucess-pic" />
       <h2>
         You scored {score} out of {questions.length}
       </h2>
       <p>
-        SHOCKED WITH THE RESULTS? Congratulations, there is still much to learn!{" "}
+        SHOCKED WITH THE RESULTS? Congratulations, there is still much to learn!
         <br />
         Do not worry. 90% of the wine connoisseurs confess that they have never
         heard of some of the Spanish varieties mentioned. <br />
@@ -28,6 +35,9 @@ function Score() {
       {/* Aca deberia ir el mensaje de Perdiste, se te acbo el t */}
 
       <button onClick={(e) => restartGame(e)}>Play Again</button>
+      <WhatsappShareButton url={shareUrl}>
+        <WhatsappIcon size={32} round={true} />
+      </WhatsappShareButton>
     </div>
   );
 }
