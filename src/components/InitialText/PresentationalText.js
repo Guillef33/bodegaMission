174febae8af92f98118ec300148fc464a212f478
Guillermo <guillef33@gmail.com>
2022-03-15 20:08:42 -0300

    ajustes de estilo

--- src/components/InitialText/PresentationalText.js
+++ src/components/InitialText/PresentationalText.js
@@ -16,7 +16,7 @@ function PresentationalText() {
         <div className="before-start-col">
           <h2>
             ​​Did you know that Spain has the largest vineyard surface in the
-            world?
+            world with more than 70 autochthonous varieties?
           </h2>
           <button className="playGameButton" onClick={(e) => StartGame(e)}>
             Play
@@ -24,31 +24,23 @@ function PresentationalText() {
         </div>
         <div className="content-start-col">
           <p>
-            Is the second in production, behind Italy, This is due, in part, to
-            the very low yields and wide spacing of the old vines planted on the
-            dry, infertile soil found in some of the Spanish wine regions. This
-            is even more impressive if you consider that the country is largely
-            made up of old mountains. There are many hidden valleys that offer
-            an enormous wealth of terroirs, where Spanish winegrowers mainly
-            work with the typical old 'bush vines' to better protect the grapes.
-            Talking about grapes, did you know that Spain has more than 70
-            autochthonous varieties?{" "}
-          </p>
-          <h4>
             We believe that Spain is the country with the greatest wealth of
             terroirs for making wine, the strongest identity and, paradoxically,
-            the most undervalued production. So our path started to be traced:
-            UNCOVER THE HIDDEN VALUE OF SPANISH WINE.
-          </h4>
+            the most undervalued production. <br />
+            <br />
+            So our path started to be traced: uncover the hidden value of
+            Spanish wine. And this is the mission you just signed for: uncover
+            what Spain has to offer. Follow the instructions and good luck!
+          </p>
         </div>
-      </div>
-      <div>
-        <img src={alert} alt="alert" />
-        <p>
-          Please open the bottle you have received & serve yourself a glass,
-          it's going to be amazing, we know but we recommend to drink in
-          moderation.
-        </p>
+        <div>
+          <img src={alert} alt="alert" />
+          <p>
+            Please open the bottle you have received & serve yourself a glass,
+            it's going to be amazing, we know but we recommend to drink in
+            moderation.
+          </p>
+        </div>
       </div>
     </div>
   );
