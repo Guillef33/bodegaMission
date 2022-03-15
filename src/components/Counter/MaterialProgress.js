a23ce0915fb5ee033c97facf86b15ccdcbca18d5
unknown <janoncarlos@gmail.com>
2022-03-15 18:52:45 -0300

    timmer 1000ms

--- src/components/Counter/MaterialProgress.js
+++ src/components/Counter/MaterialProgress.js
@@ -81,7 +81,7 @@ export default function CircularStatic({ timeIsUp, setTimeIsUp }) {
       // if (prevProgress === 0) {
       //   clearInterval(timer)
       // }
-    }, 100);
+    }, 1000);
     return () => {
       clearInterval(timer);
     };
