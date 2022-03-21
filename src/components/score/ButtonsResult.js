4340158081eeb14433104a833ea4e557fc76f67f
Guillermo <guillef33@gmail.com>
2022-03-21 09:15:05 -0300

    button copy from main

--- src/components/score/ButtonsResult.js
+++ src/components/score/ButtonsResult.js
@@ -8,25 +8,31 @@ import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 
 function ButtonsResult() {
   const [show, setShow] = useState(false);
-  const { setShowScore } = useContext(AppContext);
+
+  console.log(show);
+
+  const closeModal = () => {
+    setShow(false);
+  };
+
+  const { restartGame } = useContext(AppContext);
   console.log(window.location.href);
   console.log(show);
 
   return (
     <div className="buttons-results">
       <Link to="/game">
-        <button className="tryAgainButton" 
-        // onClick={setShowScore(false)}
-        >
+        <button className="tryAgainButton" onClick={restartGame}>
           Play Again
         </button>
       </Link>
       <button className="shareButton" onClick={() => setShow(true)}>
         Share the mission
         <BiShareAlt />
+        {show && (
+          <ShareModal show={show} setShow={setShow} closeModal={closeModal} />
+        )}
       </button>
-
-      {/* <ShareModal show={show} /> */}
     </div>
   );
 }
