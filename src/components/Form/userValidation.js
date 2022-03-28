f529e3601120f405572ee6035e174ee9469b05e0
Guillermo <guillef33@gmail.com>
2022-03-28 10:32:40 -0300

    textos ingles validaciones yup

--- src/components/Form/userValidation.js
+++ src/components/Form/userValidation.js
@@ -3,10 +3,7 @@ import * as yup from "yup";
 export const userSchema = yup.object().shape({
   name: yup
     .string()
-    .min(3, "El nombre debe tener al menos 3 caracteres")
-    .required("Ingrese un nombre valido"),
-  email: yup
-    .string()
-    .email("Ingrese un email valido")
-    .required("Ingrese un email valido"),
+    .min(3, "The name must be at least 3 characters long.")
+    .required("Add a valid name"),
+  email: yup.string().email("Add a valid email").required("Add a valid email"),
 });
