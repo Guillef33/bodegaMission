be35f16ff102d3e8656b005b0a3ef796fc7bd024
Guillermo <guillef33@gmail.com>
2022-04-01 08:56:52 -0300

    formulario arreglado

--- src/components/Form/Yupform.js
+++ src/components/Form/Yupform.js
@@ -38,6 +38,7 @@ const Yupform = () => {
   };
 
   const handleChange = (e) => {
+    e.preventDefault();
     const { value, name } = e.target;
     console.log(e.target);
     setValues({ ...values, [name]: value });
@@ -71,7 +72,7 @@ const Yupform = () => {
               name="name"
               type="text"
               placeholder="Enter your name"
-              onChange={handleChange}
+              onInput={handleChange}
               value={values.name}
             />
             {errors.name && touched.name ? (
@@ -83,7 +84,7 @@ const Yupform = () => {
               name="email"
               type="email"
               placeholder="Enter your e-mail"
-              onChange={handleChange}
+              onInput={handleChange}
               value={values.email}
             />
             {errors.email && touched.email ? (
