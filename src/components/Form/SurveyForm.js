1b1413be00b2e26722a5fa7c3e9a687f3224236e
martinrotela <martinrotela47@gmail.com>
2022-12-23 17:14:27 -0300

    fix: back button on step 8

--- src/components/Form/SurveyForm.js
+++ src/components/Form/SurveyForm.js
@@ -1,174 +1,192 @@
-import React, { useContext, useState, useRef } from "react";
-import { Navigate, useNavigate } from "react-router-dom";
-import { Formik, Field, Form } from "formik";
-import { userSchema } from "./userValidation";
-import "./Form.scss";
-import { collection, query, where, getDocs } from "firebase/firestore";
-import { addDoc } from "@firebase/firestore";
-import { db } from "../../data/config";
-import Swal from "sweetalert2";
-import { AppContext } from "../../context/AppContext";
-import emailjs from "@emailjs/browser";
+import React, { useContext, useState, useRef } from 'react';
+import { Navigate, useNavigate } from 'react-router-dom';
+import { Formik, Field, Form } from 'formik';
+import { userSchema } from './userValidation';
+import './Form.scss';
+import { collection, query, where, getDocs } from 'firebase/firestore';
+import { addDoc } from '@firebase/firestore';
+import { db } from '../../data/config';
+import Swal from 'sweetalert2';
+import { AppContext } from '../../context/AppContext';
+import emailjs from '@emailjs/browser';
 
 const SurveyForm = () => {
-  let navigate = useNavigate();
-  const form = useRef();
+    let navigate = useNavigate();
+    const form = useRef();
 
-  const { formResp } = useContext(AppContext);
+    const { formResp } = useContext(AppContext);
 
-  // Ojo con esta varoiable es un booleanoi
-  const [selectOption, setSelectOption] = useState(false);
+    // Ojo con esta varoiable es un booleanoi
+    const [selectOption, setSelectOption] = useState(false);
 
-  const initialState = {
-    name: "",
-    email: "",
-    option: "",
-    responses: [...formResp],
-  };
-
-  const [values, setValues] = useState(initialState);
-  const [emailSend, setEmailSend] = useState(false);
-
-  const handleChange = (e) => {
-    const { value, name } = e.target;
-    setValues({ ...values, [name]: value });
-  };
-
-  const handleSelect = (e) => {
-    console.log(selectOption);
-    setSelectOption(true);
-  };
-
-  const surveySubmit = async (event) => {
-    event.preventDefault();
-    let formData = {
-      name: event.target[0].value,
-      email: event.target[1].value,
-      option: event.target[2].value,
+    const initialState = {
+        name: '',
+        email: '',
+        option: '',
+        responses: [...formResp],
     };
-    console.log(formData);
-    console.log(form.current);
 
-    emailjs
-      .sendForm(
-        "gmail",
-        "template_iurxj7h",
-        form.current,
-        "user_vqPFxAk62xt1529ZNfLfd"
-      )
-      .then(
-        (result) => {
-          console.log(result.text);
-          console.log(form.current);
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
+    const [values, setValues] = useState(initialState);
+    const [emailSend, setEmailSend] = useState(false);
 
-    let isValid = await userSchema.isValid(formData);
-    isValid = true;
-    const docRef = await addDoc(collection(db, "surveys"), {
-      values,
-    });
-    setValues(initialState);
+    const handleChange = (e) => {
+        const { value, name } = e.target;
+        setValues({ ...values, [name]: value });
+    };
 
-    console.log(values);
+    const handleSelect = (e) => {
+        console.log(selectOption);
+        setSelectOption(true);
+    };
 
-    isValid &&
-      Swal.fire(
-        {
-          title: "Ready to see the correct answer? ",
-          icon: "success",
-          confirmButtonText: "Ok",
-          customClass: "button-about",
-          timer: 3500,
-        },
-        isValid && setEmailSend(docRef.id),
-        isValid && setTimeout(() => navigate("/results"), 4000)
-      );
-  };
+    const surveySubmit = async (event) => {
+        event.preventDefault();
+        let formData = {
+            name: event.target[0].value,
+            email: event.target[1].value,
+            option: event.target[2].value,
+        };
+        console.log(formData);
+        console.log(form.current);
 
-  return (
-    <div className>
-      <h3 className="form-h3-survey">
-        WHILE WE CALCULATE YOUR RESULTS, CLICK HERE TO BOOK A MEETING WITH US,
-        AND WE WILL PREPARE A SPECIAL TASTING FOR YOU.
-      </h3>
+        emailjs
+            .sendForm(
+                'gmail',
+                'template_iurxj7h',
+                form.current,
+                'user_vqPFxAk62xt1529ZNfLfd'
+            )
+            .then(
+                (result) => {
+                    console.log(result.text);
+                    console.log(form.current);
+                },
+                (error) => {
+                    console.log(error.text);
+                }
+            );
 
-      <Formik
-        validationSchema={userSchema}
-        initialValues={{ name: "", email: "", option: "" }}
-      >
-        {({ errors, touched }) => (
-          <Form className="form-wrapper" onSubmit={surveySubmit} ref={form}>
-            <label className="survey-label"> Select an option </label>
-            <Field
-              name="select"
-              as="select"
-              placeholder="Pick one option"
-              onInput={handleSelect}
-              value={values.select}
-              className="form-survey-select"
+        let isValid = await userSchema.isValid(formData);
+        isValid = true;
+        const docRef = await addDoc(collection(db, 'surveys'), {
+            values,
+        });
+        setValues(initialState);
+
+        console.log(values);
+
+        isValid &&
+            Swal.fire(
+                {
+                    title: 'Ready to see the correct answer? ',
+                    icon: 'success',
+                    confirmButtonText: 'Ok',
+                    customClass: 'button-about',
+                    timer: 3500,
+                },
+                isValid && setEmailSend(docRef.id),
+                isValid && setTimeout(() => navigate('/results'), 4000)
+            );
+    };
+
+    return (
+        <div>
+            <h3 className="form-h3-survey">
+                WHILE WE CALCULATE YOUR RESULTS, CLICK HERE TO BOOK A MEETING
+                WITH US, AND WE WILL PREPARE A SPECIAL TASTING FOR YOU.
+            </h3>
+
+            <Formik
+                validationSchema={userSchema}
+                initialValues={{ name: '', email: '', option: '' }}
             >
-              <option>Pick one option</option>
-              <option value="barcelona">
-                I want to schedule a meeting at Barcelona Wine Week
-              </option>
-              <option value="prowein">
-                I want to schedule a meeting at Prowein
-              </option>
-              <option value="meeting">
-                I want to schedule a meeting at Wine Expo{" "}
-              </option>
-              <option value="singapur">
-                I want to schedule a meeting at Prowein Singapur{" "}
-              </option>
-            </Field>
-            {errors.select && touched.select ? (
-              <p className="validation-Error">{errors.select}</p>
-            ) : null}
+                {({ errors, touched }) => (
+                    <Form
+                        className="form-wrapper"
+                        onSubmit={surveySubmit}
+                        ref={form}
+                    >
+                        <label className="survey-label">
+                            {' '}
+                            Select an option{' '}
+                        </label>
+                        <Field
+                            name="select"
+                            as="select"
+                            placeholder="Pick one option"
+                            onInput={handleSelect}
+                            value={values.select}
+                            className="form-survey-select"
+                        >
+                            <option>Pick one option</option>
+                            <option value="barcelona">
+                                I want to schedule a meeting at Barcelona Wine
+                                Week
+                            </option>
+                            <option value="prowein">
+                                I want to schedule a meeting at Prowein
+                            </option>
+                            <option value="meeting">
+                                I want to schedule a meeting at Wine Expo{' '}
+                            </option>
+                            <option value="singapur">
+                                I want to schedule a meeting at Prowein Singapur{' '}
+                            </option>
+                        </Field>
+                        {errors.select && touched.select ? (
+                            <p className="validation-Error">{errors.select}</p>
+                        ) : null}
 
-            {selectOption === true && (
-              <>
-                <label className="survey-label"> Your name </label>
-                <Field
-                  name="name"
-                  type="text"
-                  placeholder="Enter your name"
-                  onInput={handleChange}
-                  value={values.name}
-                  className="form-survey-input"
-                />
-                {errors.name && touched.name ? (
-                  <p className="validation-Error">{errors.name}</p>
-                ) : null}
-                {/* <ErrorMessage name="name" /> */}
-                <label className="survey-label"> Your email </label>
-                <Field
-                  name="email"
-                  type="email"
-                  placeholder="Enter your e-mail"
-                  onInput={handleChange}
-                  value={values.email}
-                  className="form-survey-input"
-                />
-                {errors.email && touched.email ? (
-                  <p className="validation-Error">{errors.email}</p>
-                ) : null}
-                <button
-                  className="send-form-button send-form-button-survey"
-                  type="submit"
-                >
-                  See more
-                </button>
-              </>
-            )}
-          </Form>
-        )}
-      </Formik>
-    </div>
-  );
+                        {selectOption === true && (
+                            <>
+                                <label className="survey-label">
+                                    {' '}
+                                    Your name{' '}
+                                </label>
+                                <Field
+                                    name="name"
+                                    type="text"
+                                    placeholder="Enter your name"
+                                    onInput={handleChange}
+                                    value={values.name}
+                                    className="form-survey-input"
+                                />
+                                {errors.name && touched.name ? (
+                                    <p className="validation-Error">
+                                        {errors.name}
+                                    </p>
+                                ) : null}
+                                {/* <ErrorMessage name="name" /> */}
+                                <label className="survey-label">
+                                    {' '}
+                                    Your email{' '}
+                                </label>
+                                <Field
+                                    name="email"
+                                    type="email"
+                                    placeholder="Enter your e-mail"
+                                    onInput={handleChange}
+                                    value={values.email}
+                                    className="form-survey-input"
+                                />
+                                {errors.email && touched.email ? (
+                                    <p className="validation-Error">
+                                        {errors.email}
+                                    </p>
+                                ) : null}
+                                <button
+                                    className="send-form-button send-form-button-survey"
+                                    type="submit"
+                                >
+                                    See more
+                                </button>
+                            </>
+                        )}
+                    </Form>
+                )}
+            </Formik>
+        </div>
+    );
 };
 
 export default SurveyForm;
--- src/components/surveyBox/FormContainer.js
+++ src/components/surveyBox/FormContainer.js
@@ -1,84 +1,84 @@
-import React, { useState, useEffect } from "react";
-import { useFormik } from "formik";
-import * as yup from "yup";
-import FormItemList from "./FormItemList2";
+import React, { useState, useEffect } from 'react';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
+import FormItemList from './FormItemList2';
 
 import './scss/FormContainer.scss';
-import ControlButton from "./ControlButton";
+import ControlButton from './ControlButton';
 
 import data from './data';
 
-import ReactGA4 from "react-ga4";
-
+import ReactGA4 from 'react-ga4';
 
 const FormContainer = () => {
-  const validationSchema = yup.object({
-    name: yup
-      .string("Enter your email")
-      .required("Name is required"),
-    email: yup
-      .string("Enter your email")
-      .email("Enter a valid email")
-      .required("Email is required"),
-  });
+    const validationSchema = yup.object({
+        name: yup.string('Enter your email').required('Name is required'),
+        email: yup
+            .string('Enter your email')
+            .email('Enter a valid email')
+            .required('Email is required'),
+    });
 
-  const formik = useFormik({
-    initialValues: {
-      //TODO: definir todos los datos que quiero recaudar
-      name: 'juan cruz',
-      email: 'email@email.com',
-    },
-    validationSchema: validationSchema,
-    onSubmit: (values) => {
-      //Acá va el envío a firebase
-      // alert(JSON.stringify(values, null, 2));
-    },
-  });
+    const formik = useFormik({
+        initialValues: {
+            //TODO: definir todos los datos que quiero recaudar
+            name: 'juan cruz',
+            email: 'email@email.com',
+        },
+        validationSchema: validationSchema,
+        onSubmit: (values) => {
+            //Acá va el envío a firebase
+            // alert(JSON.stringify(values, null, 2));
+        },
+    });
 
-  const [currentScreen, setCurrentScreen] = useState(0);
-  
-  const [finish, setFinish] = useState(false)
+    const [currentScreen, setCurrentScreen] = useState(0);
+    console.log(currentScreen);
 
-      useEffect(() => {
-        ReactGA4.initialize("G-6GXQ55LQ04");
-        ReactGA4.send({ hitType: "pageview", page: "/survey" });
-      }, []);
+    const [finish, setFinish] = useState(false);
 
-  return (
-    <section
-      className="box-form-container"
-      style={{
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",
-        backgroundImage: `url(${data[currentScreen].image})`,
-      }}
-    >
-      {finish ? "" : <h2>Step {currentScreen + 1} of 8</h2>}
-      <ControlButton
-        type={"back"}
-        initial={0}
-        limit={6}
-        currentScreen={currentScreen}
-        setCurrentScreen={setCurrentScreen}
-      />
-      <div className="box-form form-answers">
-        <FormItemList
-          currentScreen={currentScreen}
-          // setQuestions={setQuestions}
-          formik={formik}
-          finish={finish}
-          setFinish={setFinish}
-        />
-      </div>
-      <ControlButton
-        type={"next"}
-        initial={0}
-        limit={7}
-        currentScreen={currentScreen}
-        setCurrentScreen={setCurrentScreen}
-      />
-    </section>
-  );
+    useEffect(() => {
+        ReactGA4.initialize('G-6GXQ55LQ04');
+        ReactGA4.send({ hitType: 'pageview', page: '/survey' });
+    }, []);
+
+    return (
+        <section
+            className="box-form-container"
+            style={{
+                backgroundRepeat: 'no-repeat',
+                backgroundSize: 'cover',
+                backgroundImage: `url(${data[currentScreen].image})`,
+            }}
+        >
+            {finish ? '' : <h2>Step {currentScreen + 1} of 8</h2>}
+            <ControlButton
+                type={'back'}
+                initial={0}
+                limit={0}
+                currentScreen={currentScreen}
+                setCurrentScreen={setCurrentScreen}
+            />
+            <div className="box-form form-answers">
+                <FormItemList
+                    currentScreen={currentScreen}
+                    // setQuestions={setQuestions}
+                    formik={formik}
+                    finish={finish}
+                    setFinish={setFinish}
+                />
+            </div>
+            {currentScreen !== 7 && (
+                <ControlButton
+                    type={'next'}
+                    initial={0}
+                    limit={7}
+                    currentScreen={currentScreen}
+                    setCurrentScreen={setCurrentScreen}
+                />
+            )}
+        </section>
+    );
 };
 
 export default FormContainer;
